332ee1d9a8c6bb77e43b214346f8fa0425f247d8
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-10 20:58:58 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
 class Item(models.Model):
@@ -7,5 +7,42 @@ class Item(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.item_name)                   
-                            
+        return str(self.item_name)
+
+class NutritionalInformation(models.Model):
+    # Link to the Item model defined above
+    menu_item = models.ForeignKey(
+        Item, 
+        on_delete=models.CASCADE,
+        related_name='nutritional_info'
+    )
+    
+    # Nutritional data fields
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.item_name} - {self.calories} kcal"    
+    
+class ProductMenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name    
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    is_allergen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    # ... other fields ...
+    ingredients = models.ManyToManyField(Ingredient, related_name="menu_items")    
