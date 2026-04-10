405546aebea6c3c1a55ed4a1ec18655c64affe9e
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-10 12:43:41 +0530

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView # type: ignore
+from .models import MenuCategory
+from products.models import Item  # Changed MenuItem to Item
+from .serializers import MenuCategorySerializer, MenuItemSerializer  # Changed MenuItemSerializer to ItemSerializer
 
-# Create your views here.
+# Existing view for categories
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
+
+# New view for featured items
+class FeaturedMenuItemView(ListAPIView):
+    serializer_class = MenuItemSerializer
+
+    def get_queryset(self):
+        # Using Item model and filtering by is_featured
+        return Item.objects.filter(is_featured=True)
