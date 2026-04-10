5fd81f92e40420f2f331ebc59e2a050ce7feb82c
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-10 11:31:09 +0530

    Add files via upload

--- serializers.py
+++ serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Item
+from .models import MenuCategory, MenuItem
 
-class ItemSerializer(serializers.ModelSerializer):
+class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
-        fields = '__all__'
+        model = MenuCategory
+        fields = ['id', 'name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    # This displays the category name instead of just the ID number
+    category_name = serializers.ReadOnlyField(source='category.name')
+
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'price', 'is_featured', 'category', 'category_name']
