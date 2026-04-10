9a0ac34727e05d974fa8ac76a6c48aa0b3dd52ec
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-10 11:38:32 +0530

    Add files via upload

---
+++ orders/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers # type: ignore
+from .models import Order, OrderItem
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    item_name = serializers.ReadOnlyField(source='menu_item.name')
+
+    class Meta:
+        model = OrderItem # type: ignore
+        fields = ['item_name', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order # type: ignore
+        fields = ['id', 'created_at', 'total_price', 'items']
