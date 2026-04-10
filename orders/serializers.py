8a1cc0ff85994ba84280820bd7995a3f59ce64a8
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-10 12:27:43 +0530

    Add files via upload

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers # type: ignore
-from .models import Order, OrderItem
+from .models import Item
 
-class OrderItemSerializer(serializers.ModelSerializer):
-    item_name = serializers.ReadOnlyField(source='menu_item.name')
-
+class ItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItem # type: ignore
-        fields = ['item_name', 'quantity', 'price']
-
-class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Order # type: ignore
-        fields = ['id', 'created_at', 'total_price', 'items'] 
+        model = Item
+        fields = ['id', 'item_name', 'item_price', 'created_at']
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include # type: ignore
+from rest_framework.routers import DefaultRouter # type: ignore
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet, basename='item')
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
