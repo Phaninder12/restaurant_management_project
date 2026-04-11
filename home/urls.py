1eed81b42b3a43927dd8f87829b7eebd1175defd
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-11 09:52:43 +0530

    69717

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import AvailableTablesAPIView, MenuCategoryListView, TableDetailView,MenuItemListView
+from .views import MenuCategoryListView, TableDetailView
 
 urlpatterns = [
     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
     # Detail endpoint: e.g., /api/tables/1/
     path('api/tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
-    path('api/menu/', MenuItemListView.as_view(), name='menu-item-list'),
-    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
-]
+]    
