e6f4a59c795875d2f30de403c939958f4ecda21e
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-11 09:06:35 +0530

    69671

--- home/views.py
+++ home/views.py
@@ -1,29 +1,17 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView # type: ignore
-# Import models clearly
-from .models import MenuCategory, MenuItem as HomeMenuItem
-from products.models import Item
+from rest_framework.generics import ListAPIView # type: ignore
+from .models import MenuCategory
+from products.models import Item  # Changed MenuItem to Item
+from .serializers import MenuCategorySerializer, MenuItemSerializer  # Changed MenuItemSerializer to ItemSerializer
 
-# Import serializers
-from .serializers import (
-    MenuCategorySerializer, 
-    MenuItemSerializer, 
-    MenuItemIngredientsSerializer
-)
-
-# 1. Existing view for categories
+# Existing view for categories
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
 
-# 2. View for featured items (using products.Item)
+# New view for featured items
 class FeaturedMenuItemView(ListAPIView):
-    serializer_class = MenuItemSerializer # Ensure this is the one from home.serializers
+    serializer_class = MenuItemSerializer
 
     def get_queryset(self):
-        return Item.objects.filter(is_featured=True)
-
-# 3. View for Ingredients (using home.MenuItem)
-class MenuItemIngredientsView(RetrieveAPIView):
-    # Use the 'HomeMenuItem' alias to be safe
-    queryset = HomeMenuItem.objects.all()
-    serializer_class = MenuItemIngredientsSerializer
+        # Using Item model and filtering by is_featured
+        return Item.objects.filter(is_featured=True)  
