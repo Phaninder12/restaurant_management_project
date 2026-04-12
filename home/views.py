ab90332933fd9210ecb6671a47fd5e38bbc01d6c
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-12 08:46:13 +0530

    69723

--- home/views.py
+++ home/views.py
@@ -1,51 +1,17 @@
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .models import MenuCategory, MenuItem, Table, Restaurant
-from .serializers import (
-    MenuCategorySerializer,
-    MenuItemSerializer,
-    MenuItemIngredientsSerializer,
-    TableSerializer,
-    RestaurantSerializer,
-)
+from rest_framework.generics import ListAPIView # type: ignore
+from .models import MenuCategory
+from products.models import Item  # Changed MenuItem to Item
+from .serializers import MenuCategorySerializer, MenuItemSerializer  # Changed MenuItemSerializer to ItemSerializer
 
-
+# Existing view for categories
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
 
-
-class MenuItemIngredientsView(RetrieveAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemIngredientsSerializer
-
-
-def home_page(request):
-    featured_dishes = MenuItem.objects.get_top_selling_items(3)
-    return render(request, 'home/index.html', {'featured_dishes': featured_dishes})
-
-
-class TableDetailView(RetrieveAPIView):
-    queryset = Table.objects.all()
-    serializer_class = TableSerializer
-
-
-class RestaurantInfoAPIView(generics.ListAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantSerializer
-
-
-class MenuItemListView(generics.ListAPIView):
+# New view for featured items
+class FeaturedMenuItemView(ListAPIView):
     serializer_class = MenuItemSerializer
 
     def get_queryset(self):
-        return MenuItem.objects.all()
-
-
-class AvailableTablesAPIView(generics.ListAPIView):
-    serializer_class = TableSerializer
-
-    def get_queryset(self):
-        return Table.objects.filter(is_available=True)
-       
+        # Using Item model and filtering by is_featured
+        return Item.objects.filter(is_featured=True) 
