87a286b8805b036379b27d0e904f73036cba14e5
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 10:03:28 +0530

    70120

--- home/serializers.py
+++ home/serializers.py
@@ -1,64 +1,15 @@
 from rest_framework import serializers
-from .models import MenuCategory, MenuItem, Ingredient, Table, Restaurant, ContactFormSubmission, UserReview
-
+from .models import MenuCategory, MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
-        fields = ['id', 'name', 'description']
-
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = ['id', 'name', 'is_allergen']
-
+        fields = ['id', 'name']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, read_only=True)
+    # This displays the category name instead of just the ID number
+    category_name = serializers.ReadOnlyField(source='category.name')
 
     class Meta:
         model = MenuItem
-        fields = ['id', 'name', 'description', 'price', 'cuisine', 'ingredients', 'is_daily_special']
-
-
-class MenuItemIngredientsSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = MenuItem
-        fields = ['id', 'name', 'ingredients']
-
-
-class TableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Table
-        fields = ['id', 'number', 'capacity', 'is_available']
-
-
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = ['id', 'name', 'address', 'has_delivery']
-
-
-class ContactFormSubmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactFormSubmission
-        fields = ['id', 'name', 'email', 'message', 'created_at']
-        read_only_fields = ['id', 'created_at']
-
-
-class UserReviewSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-    menu_item = serializers.ReadOnlyField(source='menu_item.name')
-
-    class Meta:
-        model = UserReview
-        fields = ['id', 'user', 'menu_item', 'rating', 'comment', 'review_date']
-        read_only_fields = ['id', 'review_date']
-
-    def validate_rating(self, value):
-        if not (1 <= value <= 5):
-            raise serializers.ValidationError("Rating must be between 1 and 5.")
-        return value
+        fields = ['id', 'name', 'price', 'is_featured', 'category', 'category_name']           
