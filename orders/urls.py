9b327ab2e3d0c5f2eb636e6b748170e221511d6d
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:13:23 +0530

    Add files via upload

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
-]     
+  path('history/', OrderHistoryListView.as_view(), name='order-history'),
+  path('orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order_detail_api'),
+  path('orders/<int:pk>/status/', OrderStatusUpdateView.as_view(), name='order-status-update'),
+  path('payment-methods/', PaymentMethodListView.as_view(), name='payment-methods'),
+]
