55ce960e530cb06d2fdf83fd77046f2fb1ef2812
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:10:36 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,8 @@
 from django.urls import path
-from .views import (
-    AvailableTablesAPIView,
-    MenuCategoryListView,
-    TableDetailView,
-    MenuItemListView,
-    RestaurantInfoAPIView,
-    ContactFormSubmissionCreateAPIView,
-)
+from .views import *
 
 urlpatterns = [
-    path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
-    path('restaurant-info/', RestaurantInfoAPIView.as_view(), name='restaurant-info'),
-    # Detail endpoint: e.g., /api/tables/1/
-    path('api/tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
-    path('api/menu/', MenuItemListView.as_view(), name='menu-item-list'),
-    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
-    path('contact/', ContactFormSubmissionCreateAPIView.as_view(), name='contact-form-submit'),
+  path('history/', OrderHistoryListView.as_view(), name='order-history'),
+  path('orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order_detail_api'),
+  path('payment-methods/', PaymentMethodListView.as_view(), name='payment-methods'),
 ]
