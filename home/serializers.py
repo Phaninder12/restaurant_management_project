4c8b5e5b6b5f117d9cba7a4020dbe1d0f3c2cf09
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:35:15 +0530

    70116

--- home/serializers.py
+++ home/serializers.py
@@ -1,49 +1,15 @@
 from rest_framework import serializers
-from .models import MenuCategory, MenuItem, Ingredient, Table, Restaurant, ContactFormSubmission
-
+from .models import MenuCategory, MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id', 'name']
 
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = ['id', 'name', 'is_allergen']
-
-
 class MenuItemSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, read_only=True)
+    # This displays the category name instead of just the ID number
+    category_name = serializers.ReadOnlyField(source='category.name')
 
     class Meta:
         model = MenuItem
-        fields = ['id', 'name', 'description', 'price', 'ingredients']
-
-
-class MenuItemIngredientsSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = MenuItem
-        fields = ['id', 'name', 'ingredients']
-
-
-class TableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Table
-        fields = ['id', 'number', 'capacity', 'is_available']
-
-
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = ['id', 'name', 'address', 'has_delivery']
-
-
-class ContactFormSubmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactFormSubmission
-        fields = ['id', 'name', 'email', 'message', 'created_at']
-        read_only_fields = ['id', 'created_at']
+        fields = ['id', 'name', 'price', 'is_featured', 'category', 'category_name']     
