7b8177c1a25d6354ef19ec497cb50fbba990e676
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:39:17 +0530

    70115

--- home/urls.py
+++ home/urls.py
@@ -1,25 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    AvailableTablesAPIView,
-    DailySpecialsView,
-    MenuCategoryViewSet,
-    TableDetailView,
-    MenuItemListView,
-    RestaurantInfoAPIView,
-    ContactFormSubmissionCreateAPIView,
-)
-
-router = DefaultRouter()
-router.register(r'menu-categories', MenuCategoryViewSet)
+from django.urls import path
+from .views import MenuCategoryListView, TableDetailView
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('daily-specials/', DailySpecialsView.as_view(), name='daily-specials'),
-    path('restaurant-info/', RestaurantInfoAPIView.as_view(), name='restaurant-info'),
+    path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
     # Detail endpoint: e.g., /api/tables/1/
     path('api/tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
-    path('api/menu/', MenuItemListView.as_view(), name='menu-item-list'),
-    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
-    path('contact/', ContactFormSubmissionCreateAPIView.as_view(), name='contact-form-submit'),
-]
+]          
