501beb848df0a4ccf62ada06807abbcec63e7f27
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:26:58 +0530

    Add files via upload

--- home/models.py
+++ home/models.py
@@ -9,6 +9,13 @@ class MenuCategory(models.Model):
         return self.name
 
 
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     is_allergen = models.BooleanField(default=False)
@@ -26,6 +33,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True, blank=True, related_name='menu_items')
     ingredients = models.ManyToManyField('Ingredient', related_name='menu_items', blank=True)
 
     objects = MenuItemManager()
