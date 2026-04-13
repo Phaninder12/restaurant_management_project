559ad6f14220646bf64930f774dceb572f496809
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-13 09:10:57 +0530

    70096

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-  path('history/', OrderHistoryListView.as_view(), name='order-history'),
-  path('orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order_detail_api'),
-  path('payment-methods/', PaymentMethodListView.as_view(), name='payment-methods'),
-]
+    
+]     
