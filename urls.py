9a9b3ffbbab032de3d855ac147d98f3f1f5fa9ff
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-14 10:24:31 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,36 +1,11 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    AvailableTablesAPIView,
-    DailySpecialsView,
-    MenuCategoryViewSet,
-    MenuCategoryNameListView,
-    MenuItemReviewsView,
-    TableDetailView,
-    MenuItemListView,
-    RestaurantInfoAPIView,
-    ContactFormSubmissionCreateAPIView,
-    UserReviewCreateView,
-    UserReviewListView,
-    update_menu_item_availability,
-    get_restaurant_info,
-)
-
-router = DefaultRouter()
-router.register(r'menu-categories', MenuCategoryViewSet)
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('reviews/', UserReviewListView.as_view(), name='user-reviews-list'),
-    path('reviews/create/', UserReviewCreateView.as_view(), name='create-review'),
-    path('menu-items/<int:menu_item_id>/reviews/', MenuItemReviewsView.as_view(), name='menu-item-reviews'),
-    path('menu-items/<int:menu_item_id>/availability/', update_menu_item_availability, name='update-menu-item-availability'),
-    path('menu-categories/names/', MenuCategoryNameListView.as_view(), name='menu-category-names'),
-    path('daily-specials/', DailySpecialsView.as_view(), name='daily-specials'),
-    path('restaurant-info/', get_restaurant_info, name='restaurant-info'),
-    # Detail endpoint: e.g., /api/tables/1/
-    path('api/tables/<int:pk>/', TableDetailView.as_view(), name='table-detail'),
-    path('api/menu/', MenuItemListView.as_view(), name='menu-item-list'),
-    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
-    path('contact/', ContactFormSubmissionCreateAPIView.as_view(), name='contact-form-submit'),
+  path('history/', OrderHistoryListView.as_view(), name='order-history'),
+  path('<int:pk>/', OrderDetailAPIView.as_view(), name='order_detail_api'),
+  path('<str:order_id>/status/', OrderStatusRetrieveView.as_view(), name='order-status-retrieve'),
+  path('<int:pk>/status/update/', OrderStatusUpdateView.as_view(), name='order-status-update'),
+  path('<int:order_id>/status/get/', get_order_status, name='get-order-status'),
+  path('payment-methods/', PaymentMethodListView.as_view(), name='payment-methods'),
 ]
