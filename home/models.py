43b53775481340fc1bfdf9dc8c478287044519b7
Phaninder12 <119859519+Phaninder12@users.noreply.github.com>
2026-04-17 09:07:53 +0530

    Add files via upload

--- home/models.py
+++ home/models.py
@@ -8,6 +8,9 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Menu Categories"
+
     def __str__(self):
         return self.name
 
